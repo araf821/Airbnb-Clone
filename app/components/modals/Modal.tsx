e6ff36591fb3e0072615991c2994619d16fc730c
araf821 <araf.ahmed200@gmail.com>
2023-04-15 03:40:58 -0400

    implemented google & github auth buttons

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -160,6 +160,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
