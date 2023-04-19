19d8fe698b3cdb4be1ae29c632b81dede91e6fd3
araf821 <araf.ahmed200@gmail.com>
2023-04-19 01:54:36 -0400

    implemented location mapping + marking

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,7 +7,7 @@ import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
@@ -33,6 +33,7 @@ const Map: React.FC<MapProps> = ({ center }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
