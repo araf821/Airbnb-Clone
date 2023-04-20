4342ea018d2ff09e01a394ac6f85085ec8fe656e
araf821 <araf.ahmed200@gmail.com>
2023-04-20 04:02:27 -0400

    initialized empty state component for displaying when no listings are available

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
         <RentModal />
         {/* <Modal isOpen title="asdf" actionLabel="Submit" /> */}
         <Navbar currentUser={currentUser} />
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
