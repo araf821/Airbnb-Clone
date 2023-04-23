4765f762cd4dba0a5a3842c3f9f1e1d1d776a0a6
araf821 <araf.ahmed200@gmail.com>
2023-04-22 23:58:15 -0400

    implemented listingInfo comp + initialized listinCategory comp

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface ListingCategoryProps {
+  icon: IconType;
+  label: string;
+  description: string;
+}
+
+const ListingCategory: React.FC<ListingCategoryProps> = ({
+  icon: Icon,
+  label,
+  description,
+}) => {
+  return <div>ListingCategory</div>;
+};
+export default ListingCategory;
