e687215a0542dcf70451eca7635b7c1ae775f01e
araf821 <araf.ahmed200@gmail.com>
2023-04-23 21:45:48 -0400

    initialized trips page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from "@/app/hooks/useLoginModal";
 import { signOut } from "next-auth/react";
 import { SafeUser } from "@/app/types";
 import useRentModal from "@/app/hooks/useRentModal";
+import { useRouter } from "next/navigation";
 
 interface UserProps {
   currentUser?: SafeUser | null;
@@ -18,6 +19,8 @@ const UserMenu: React.FC<UserProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
+
+  const router = useRouter();
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -83,7 +86,10 @@ const UserMenu: React.FC<UserProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My Trips" />
+                <MenuItem
+                  onClick={() => router.push("/trips")}
+                  label="My Trips"
+                />
                 <MenuItem onClick={() => {}} label="My Favourites" />
                 <MenuItem onClick={() => {}} label="My Reservations" />
                 <MenuItem onClick={() => {}} label="My Properties" />
---
+++ app/trips/TripsClient.tsx
@@ -0,0 +1,14 @@
+import { SafeReservation, SafeUser } from "../types";
+
+interface TripsClientProps {
+  reservations: SafeReservation[];
+  currentUser: SafeUser;
+}
+
+const TripsClient: React.FC<TripsClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  return <div>TripsClient</div>;
+};
+export default TripsClient;
